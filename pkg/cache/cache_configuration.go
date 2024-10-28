75ab4f565072c1b9a6243f53a7b7a6ef66ca445f
jeferson <jeferson.smoraes@outlook.com>
2024-10-28 15:53:25 -0300

    tests

--- pkg/cache/cache_configuration.go
+++ pkg/cache/cache_configuration.go
@@ -7,7 +7,10 @@ import (
 )
 
 var (
-	ErrorCacheProviderEnv = errors.New("could not read UFOMS_CACHE_PROVIDER ENV")
+	ErrorCacheProviderEnv   = errors.New("could not read UFOMS_CACHE_PROVIDER ENV")
+	ErrorCacheHostEnv       = errors.New("could not read UFOMS_CACHE_HOST ENV")
+	ErrorCacheDataBaseEnv   = errors.New("could not read UFOMS_CACHE_DATA_BASE ENV")
+	ErrorCacheBufferSizeEnv = errors.New("could not read UFOMS_CACHE_BUFFER_SIZE ENV")
 )
 
 type Configuration struct {
@@ -20,7 +23,7 @@ type Configuration struct {
 	CacheEnableTls          bool   `env:"UFOMS_CACHE_ENABLE_TLS"`
 	CacheInsecureSkipVerify bool   `env:"UFOMS_CACHE_INSECURE_SKIP_VERIFY"`
 	CacheBufferSize         int    `env:"UFOMS_CACHE_BUFFER_SIZE"`
-	Channels                string `env:"UFOMS_NEW_ORDER_CHANNEL"`
+	Channels                string `env:"UFOMS_SUBSCRIBE_TO_CHANNELS"`
 }
 
 func LoadEnvironments() (*Configuration, []error) {
@@ -44,8 +47,21 @@ func LoadEnvironments() (*Configuration, []error) {
 func (c *Configuration) validateEnvironmentVariables() []error {
 
 	errs := make([]error, 0)
+
 	if c.CacheProvider == "" {
 		errs = append(errs, ErrorCacheProviderEnv)
+	}
+
+	if c.CacheHost == "" {
+		errs = append(errs, ErrorCacheHostEnv)
+	}
+
+	if c.CacheDataBase < 1 {
+		errs = append(errs, ErrorCacheDataBaseEnv)
+	}
+
+	if c.CacheBufferSize < 1 {
+		errs = append(errs, ErrorCacheBufferSizeEnv)
 	}
 
 	return errs
