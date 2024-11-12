c5514fb0f9874f5b4a5a28f3f0e2d8064bad96e0
jeferson <jeferson.smoraes@outlook.com>
2024-11-12 15:02:56 -0300

    ajuste cache

--- pkg/cache/cache_configuration.go
+++ pkg/cache/cache_configuration.go
@@ -12,16 +12,16 @@ var (
 )
 
 type CacheConfiguration struct {
-	CacheProvider           string `env:"UFOMS_CACHE_PROVIDER"`
-	CachePrefix             string `env:"UFOMS_CACHE_PREFIX"`
-	CacheHost               string `env:"UFOMS_CACHE_HOST"`
-	CacheDataBase           int    `env:"UFOMS_CACHE_DATA_BASE"`
-	CachePassword           string `env:"UFOMS_CACHE_PASSWORD"`
-	CacheUserName           string `env:"UFOMS_CACHE_USER_NAME"`
-	CacheEnableTls          bool   `env:"UFOMS_CACHE_ENABLE_TLS"`
-	CacheInsecureSkipVerify bool   `env:"UFOMS_CACHE_INSECURE_SKIP_VERIFY"`
-	CacheBufferSize         int    `env:"UFOMS_CACHE_BUFFER_SIZE"`
-	Channels                string `env:"UFOMS_SUBSCRIBE_TO_CHANNELS"`
+	CacheProvider           string
+	CachePrefix             string
+	CacheHost               string
+	CacheDataBase           int
+	CachePassword           string
+	CacheUserName           string
+	CacheEnableTls          bool
+	CacheInsecureSkipVerify bool
+	CacheBufferSize         int
+	Channels                string
 }
 
 func Load(c *CacheConfiguration) (*CacheConfiguration, []error) {
